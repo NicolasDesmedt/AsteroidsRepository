4c9834e0c7a127b2161bbcf0a6e6a46cb16379c4
Nicolas Desmedt <nicolas.desmedt@student.kuleuven.be>
2017-03-29 11:46:07 +0200

    World eerste deel

--- OGP1617-Asteroids/src/asteroids/model/World.java
+++ OGP1617-Asteroids/src/asteroids/model/World.java
@@ -1,5 +1,49 @@
 package asteroids.model;
 
-public class World {
+import be.kuleuven.cs.som.annotate.*;
 
+/**
+ * 
+ * @author Nicolas
+ *
+ */
+public class World{
+	
+	/**
+	 * 
+	 * @param width
+	 * @param height
+	 */
+	public World(double width, double height) {
+		if (! ((width >= 0) && (width <= upperBound)) )
+			width = upperBound;
+		if (! ((height >= 0) && (height <= upperBound)) )
+			height = upperBound;
+		this.width = width;
+		this.height = height;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@Basic @Immutable
+	public double getUpperBound() {
+		return World.upperBound;
+	}
+	
+	/**
+	 * 
+	 */
+	private static final double upperBound = Double.MAX_VALUE;
+	
+	/**
+	 * 
+	 */
+	private final double width;
+	
+	/**
+	 * 
+	 */
+	private final double height;
 }
