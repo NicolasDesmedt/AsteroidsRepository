a7e94b53f303ddfbf137f2a9378da0cce1fc0ae6
Lucas Desard <lucs-rules@hotmail.com>
2017-04-05 00:06:04 +0200

    1.2 en 1.3 volledig afgewerkt + begonnen testen

---
+++ OGP1617-Asteroids/tests/asteroids/tests/BulletTest.java
@@ -0,0 +1,15 @@
+package asteroids.tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import asteroids.model.Bullet;
+
+public class BulletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
